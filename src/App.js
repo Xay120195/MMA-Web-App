514b4e1ca2fb817c112c45f641d62d22add104d7
adriansilva-lop <adrian.silva@lophils.com>
2021-10-26 17:23:55 +0800

    install tailwindcss

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Routes from './Routes';
 import Amplify from "@aws-amplify/core";
 import PubSub from "@aws-amplify/pubsub";
 import awsmobile from "./aws-exports";
-
+import './assets/styles/styles.css';
 Amplify.configure(awsmobile);
 PubSub.configure(awsmobile);
 
