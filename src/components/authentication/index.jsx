8513d7115c1ac29832dfe3eb65b1fb28ac7865c0
Adrian <adrian.silva@lophils.com>
2021-10-27 09:18:07 +0800

    Test UI - add basic header on login page

--- src/components/authentication/index.jsx
+++ src/components/authentication/index.jsx
@@ -4,6 +4,21 @@ import { AmplifyAuthenticator, AmplifySignUp, AmplifySignIn } from '@aws-amplify
 import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 import '../../assets/styles/Auth.css'
 
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+//import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+
+const navigation = [
+  { name: 'Contact', href: '/contact', current: false },
+  { name: 'Login', href: '/login', current: true },
+  { name: 'Signup', href: '/signup', current: false }
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+
 const Authentication = () => {
     const [authState, setAuthState] = React.useState();
     const [user, setUser] = React.useState();
@@ -20,12 +35,84 @@ const Authentication = () => {
         <Redirect to="/dashboard" />
       </>
     ) : (
+        <>
+        <Disclosure as="nav">
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+                {/* Mobile menu button*/}
+                {/* <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div> */}
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex-shrink-0 flex items-center">
+                  {/* <img
+                    className="block lg:hidden h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    alt="Workflow"
+                  />
+                  <img
+                    className="hidden lg:block h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                    alt="Workflow"
+                  /> */}
+                </div>
+                <div className="hidden sm:block sm:ml-6 items-center">
+                  <div className="flex space-x-4">
+                    <h1>AFFIDAVITS &amp; RFI </h1>
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                
+              {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'px-3 py-2 rounded-md text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                
+              </div>
+            </div>
+          </div>
 
-        // <figure class="md:flex bg-gray-100 rounded-xl p-8 md:p-0">
-        //     <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-        //         <p> Test </p>
-        //         </div>
-        //     <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
 
       <AmplifyAuthenticator>
         <AmplifySignIn 
@@ -42,8 +129,7 @@ const Authentication = () => {
           ]}
         />
       </AmplifyAuthenticator>
-    //   </div>
-    //   </figure>
+    </>
   );
 }
 
