24db26e99b9a400a395fb4b1f5fc226c1fd07d45
Adrian <adrian.silva@lophils.com>
2021-11-08 17:58:56 +0800

    FEATURE: Sidebar - Remove Matters Tab

--- src/components/navigation/SidebarData.jsx
+++ src/components/navigation/SidebarData.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as FaIcons from 'react-icons/fa';
+// import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
 import { AppRoutes } from "../../constants/AppRoutes";
@@ -11,12 +11,12 @@ export const SidebarData = [
     icon: <AiIcons.AiFillHome />,
     cName: 'nav-text'
   },
-  {
-    title: 'Matters',
-    path: AppRoutes.MATTERS,
-    icon: <IoIcons.IoIosPaper />,
-    cName: 'nav-text'
-  },
+  // {
+  //   title: 'Matters',
+  //   path: AppRoutes.MATTERS,
+  //   icon: <IoIcons.IoIosPaper />,
+  //   cName: 'nav-text'
+  // },
   {
     title: 'Users Access',
     path: AppRoutes.USERACCESS,
