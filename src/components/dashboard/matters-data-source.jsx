d5263eb699a71642173c7400ef0790640bfb76ce
Adrian <adrian.silva@lophils.com>
2021-11-09 17:11:07 +0800

    FEATURE: Dashboard - Create Dummy Matters

---
+++ src/components/dashboard/matters-data-source.jsx
@@ -0,0 +1,37 @@
+export const matters = [{
+    id: 6956,
+    name: 'Matter 1',
+    matter_number: '{M1-6956/155}',
+    client_id: 155,
+    client_name: 'Client Name 1',
+    substantially_responsible: {
+        id: 2,
+        name: 'Adrian Silva',
+        email: 'adrian.silva@lophils.com',
+        profile_picture: 'https://as2.ftcdn.net/v2/jpg/00/63/06/45/1000_F_63064599_c2YEM1vnauuB1eenrhrAhhaSNwUHx2vQ.jpg?auto=compress&cs=tinysrgb&h=650&w=940'
+    }
+},{
+    id: 8956,
+    name: 'Test Matter',
+    matter_number: '{TM-8956/210}',
+    client_id: 210,
+    client_name: 'Client Name 210',
+    substantially_responsible: {
+        id: 2,
+        name: 'Adrian Silva',
+        email: 'adrian.silva@lophils.com',
+        profile_picture: 'https://as1.ftcdn.net/v2/jpg/03/64/62/36/1000_F_364623643_58jOINqUIeYmkrH7go1smPaiYujiyqit.jpg?auto=compress&cs=tinysrgb&h=650&w=940'
+    }
+},{
+    id: 1816,
+    name: 'Zeng 5626',
+    matter_number: '{Z6-1816/210}',
+    client_id: 210,
+    client_name: 'Zeng Zac Homes',
+    substantially_responsible: {
+        id: 2,
+        name: 'Adrian Silva',
+        email: 'adrian.silva@lophils.com',
+        profile_picture: 'https://as1.ftcdn.net/v2/jpg/00/53/01/86/1000_F_53018616_nxpsx4iXIGTMMkfCuusbrd0jIqupmhcP.jpg?auto=compress&cs=tinysrgb&h=650&w=940'
+    }
+}]
