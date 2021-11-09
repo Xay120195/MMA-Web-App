2cfa84a9beed9dc3367c18999147225717c92e9a
Adrian <adrian.silva@lophils.com>
2021-11-09 17:12:17 +0800

    FEATURE: Dashboard - Add logic for Dashboard features

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,114 +1,92 @@
-import React, { Component } from "react";
+import React, { useState, useEffect }  from 'react';
 import * as IoIcons from 'react-icons/io';
 import Navbar from '../navigation';
+import imgDocs from '../../assets/images/docs.svg';
+import {Welcome} from './welcome'
+import {matters} from './matters-data-source'
+import { MattersList } from './matters-list'
+import { Auth } from "aws-amplify";
 
 export default function Dashboard() {
-    return (
+    const [userInfo, setuserInfo] = useState(null);
+
+    useEffect(() => {
+      let getUser = async() => {
+        try {
+          let user = await Auth.currentAuthenticatedUser();
+          await setuserInfo(user);
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      getUser();
+    },[]);
+
+    return userInfo ? (
       <>
       <Navbar />
         <div className="p-5 font-sans">
-            <div className="relative bg-gray-100 p-5 sm:p-6 rounded-sm overflow-hidden mb-8 " >
-                <h3
-                  className={
-                    "font-semi-bold text-3xl"
-                  }
-                >
-                  Welcome Back,<span className={"font-bold"}>Thomas</span> 
-                </h3>
-                <p className="text-gray-400 text-sm" >You have 2 Matters on your list.</p>
-                <p className="text-gray-400 text-sm" >To start adding, type in the name and click the add button below.</p>
-                <div className="grid grid-flow-col grid-cols-6">
-                    <div className="pr-2" >
-                        <div className="relative flex w-full flex-wrap items-stretch mb-3 py-5">
-                        <span className="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 py-3">
-                            <IoIcons.IoIosFolder/>
-                        </span>
-                        <input type="text" placeholder="Matter" className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pl-10"/>
+            <div className="relative bg-gray-100 px-12 py-8 sm:px-12 sm:py-8 rounded-sm overflow-hidden mb-8">
+
+                <div className="grid grid-cols-4 gap-4">
+                    <div className="col-span-3">
+                        
+                        <Welcome user={userInfo} matters={matters} />
+                        <p className="text-gray-400 text-sm" >To start adding, type in the name and click the add button below.</p>
+                        <div className="grid grid-flow-col grid-cols-3">
+                            <div className="pr-2" >
+                                <div className="relative flex w-full flex-wrap items-stretch mt-3 pt-5">
+                                <span className="z-10 h-full leading-snug font-normal text-center text-blueGray-300 absolute left-3 bg-transparent rounded text-base items-center justify-center w-8 py-3">
+                                    <IoIcons.IoIosFolder/>
+                                </span>
+                                <input type="text" placeholder="Matter" className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pl-10"/>
+                                </div>
+                            </div>
+                            <div className="pr-2" >
+                                <div className="relative flex w-full flex-wrap items-stretch mt-3 pt-5">
+                                <span className="z-10 h-full leading-snug font-normal text-center text-blueGray-300 absolute left-3 bg-transparent rounded text-base items-center justify-center w-8 py-3">
+                                    <IoIcons.IoIosPerson/>
+                                </span>
+                                <input type="text" placeholder="Client" className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pl-10"/>
+                                </div>
+                            </div>
+                            <div className="pr-2" >
+                                <div className="relative flex w-full flex-wrap items-stretch mt-3 pt-5">
+                                <button className="bg-gray-600 hover:bg-gray-500 text-gray-50 font-bold py-2.5 px-4 rounded items-center">+</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="pr-2" >
-                        <div className="relative flex w-full flex-wrap items-stretch mb-3 py-5">
-                        <span className="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 py-3">
-                            <IoIcons.IoIosPerson/>
-                        </span>
-                        <input type="text" placeholder="Client" className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pl-10"/>
-                        </div>
-                    </div>
-                    <div className="pr-2" >
-                        <div className="relative flex w-full flex-wrap items-stretch mb-3 py-5">
-                        <button className="bg-gray-600 hover:bg-gray-500 text-gray-50 font-bold py-2.5 px-4 rounded items-center">+</button>
-                        </div>
+                    <div className="w-3/5 place-self-end">
+                        <img src={imgDocs} alt="rightside-illustration" />
                     </div>
                 </div>
             </div>
+            
             <div className="pr-2" >
                 <div className="relative flex w-full flex-wrap items-stretch mb-3 py-5">
-                <span className="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 py-3">
+                <span className="z-10 h-full leading-snug font-normal text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 py-3">
                     <IoIcons.IoIosSearch/>
                 </span>
-                <input type="text" placeholder="Type your question or search for keywords..." className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pl-10"/>
+                <input type="text" placeholder="Type your question or search for keywords..." className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pl-10"/>
                 </div>
             </div>
 
-            <div className="grid grid-flow-col grid-cols-4">
-                <div className="pr-10" >
-                    
-                    <div className="w-full h-42 flex bg-white dark:bg-gray-800 rounded-lg border border-gray-400 mb-6 py-5 px-4">
-                        <div>
-                            <h4 tabIndex="0" className="focus:outline-none text-gray-800 dark:text-gray-100 font-bold mb-3">Matter 2</h4>
-                            <p tabIndex="0" className="focus:outline-none text-gray-800 dark:text-gray-100 text-sm">Lorem Ipsum</p>
-                            <br/>
-                            <div className="grid grid-flow-col grid-cols-4 auto-cols-auto">
-                                
-                                <div className="-space-x-4">
-                                    <img className="relative z-30 inline object-cover w-8 h-8 border-2 border-white rounded-full" src="https://as2.ftcdn.net/v2/jpg/00/63/06/45/1000_F_63064599_c2YEM1vnauuB1eenrhrAhhaSNwUHx2vQ.jpg?auto=compress&cs=tinysrgb&h=650&w=940" alt="Profile image"/>
-                                    <img className="relative z-20 inline object-cover w-8 h-8 border-2 border-white rounded-full" src="https://as1.ftcdn.net/v2/jpg/00/53/01/86/1000_F_53018616_nxpsx4iXIGTMMkfCuusbrd0jIqupmhcP.jpg?auto=compress&cs=tinysrgb&h=650&w=940" alt="Profile image"/>
-                                    <img className="relative z-10 inline object-cover w-8 h-8 border-2 border-white rounded-full" src="https://as1.ftcdn.net/v2/jpg/03/64/62/36/1000_F_364623643_58jOINqUIeYmkrH7go1smPaiYujiyqit.jpg?auto=compress&cs=tinysrgb&h=650&w=940" alt="Profile image"/>
-                                </div>
-                                <div></div>
-                                <div></div>
-                                <div>
-                                    <p tabIndex="0" className="self-end focus:outline-none text-xs text-gray-400">2 Nov 2021</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                
-                </div>
-                <div className="pr-10" >
-                    
-                    <div className="w-full h-42 flex bg-white dark:bg-gray-800 rounded-lg border border-gray-400 mb-6 py-5 px-4">
-                        <div>
-                            <h4 tabIndex="0" className="focus:outline-none text-gray-800 dark:text-gray-100 font-bold mb-3">Matter 2</h4>
-                            <p tabIndex="0" className="focus:outline-none text-gray-800 dark:text-gray-100 text-sm">Lorem Ipsum</p>
-                            <br/>
-                            <div className="grid grid-flow-col grid-cols-4 auto-cols-auto">
-                                
-                                <div className="-space-x-4">
-                                    <img className="relative z-30 inline object-cover w-8 h-8 border-2 border-white rounded-full" src="https://as2.ftcdn.net/v2/jpg/00/63/06/45/1000_F_63064599_c2YEM1vnauuB1eenrhrAhhaSNwUHx2vQ.jpg?auto=compress&cs=tinysrgb&h=650&w=940" alt="Profile image"/>
-                                    <img className="relative z-20 inline object-cover w-8 h-8 border-2 border-white rounded-full" src="https://as1.ftcdn.net/v2/jpg/00/53/01/86/1000_F_53018616_nxpsx4iXIGTMMkfCuusbrd0jIqupmhcP.jpg?auto=compress&cs=tinysrgb&h=650&w=940" alt="Profile image"/>
-                                    <img className="relative z-10 inline object-cover w-8 h-8 border-2 border-white rounded-full" src="https://as1.ftcdn.net/v2/jpg/03/64/62/36/1000_F_364623643_58jOINqUIeYmkrH7go1smPaiYujiyqit.jpg?auto=compress&cs=tinysrgb&h=650&w=940" alt="Profile image"/>
-                                </div>
-                                <div></div>
-                                <div></div>
-                                <div>
-                                    <p tabIndex="0" className="self-end focus:outline-none text-xs text-gray-400">2 Nov 2021</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            <div className="grid grid-cols-4 gap-4">
+                { matters.map((matter, index) => 
+                    <MattersList key={index} index={index} matter={matter} view={'grid'} />
+                )}
+            </div>
 
-                </div>
-                <div className="pr-2" >
-                    
-                </div>
-                <div className="pr-2" >
-                    
-                </div>
+            <div className="grid grid-flow-row auto-rows-max">
+                { matters.map((matter, index) => 
+                    <MattersList key={index} index={index} matter={matter} view={'list'} />
+                )}
             </div>
         </div>
       </>
+    ):(
+        <p>Please wait ...</p>
     );
   }
-
 
