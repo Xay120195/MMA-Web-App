c8b5cd9567733ebf74bbe64bf33657f2e07e4ad5
Adrian <adrian.silva@lophils.com>
2021-11-11 17:47:25 +0800

    FEATURE: Navigation - Hide Sidebar on Authentication Pages

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {FaBars} from 'react-icons/fa';
 import { Link, useHistory } from 'react-router-dom';
 import { Auth } from "aws-amplify";
@@ -9,16 +9,21 @@ import Sidebar from '../sidebar';
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
+  const [location, setlocation] = useState(window.location.pathname);
+  const [userInfo, setuserInfo] = useState(null);
+  
   
   let history = useHistory();
 
   const clickLogout = async (e) => {
     e.preventDefault();
     if(window.confirm("Are you sure you want to logout?")){
+      setSidebar(false);
       try {
         await Auth.signOut();
         console.log('Sign out completed.');
         history.push("/");
+        
       } catch (error) {
         console.log('Error signing out: ', error);
       }
@@ -27,17 +32,40 @@ function Navbar() {
     }
   }
 
-  return (
+  useEffect(() => {
+    // setSidebar(false); -- identify if the user changed the location/path
+  }, []);
+
+  history.listen((location) => {
+    setlocation(location.pathname);
+  })
+
+  useEffect(() => {
+    let getUser = async() => {
+      try {
+        let user = await Auth.currentAuthenticatedUser();
+        await setuserInfo(user);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getUser();
+  },[]);
+
+
+  return(
     <>
+    {location !== '/' && 
       <IconContext.Provider value={{ color: '#fff' }}>
         <div className='navbar'>
           <Link to='#' className='menu-bars bg-gray-400' >
             <FaBars onClick={showSidebar} />
           </Link>
         </div>
-        { sidebar && <Sidebar showSidebar={showSidebar} clickLogout={clickLogout}/> }
+        { sidebar && <Sidebar showSidebar={showSidebar} userInfo={userInfo} clickLogout={clickLogout}/> }
         
       </IconContext.Provider>
+      }
     </>
   );
 }
