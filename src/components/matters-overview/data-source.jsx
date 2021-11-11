6a480c81a375d71757396a424b42dc6314323687
Adrian <adrian.silva@lophils.com>
2021-11-11 10:23:55 +0800

    FEATURE: Matters Overview - Add Dummy Datasource

---
+++ src/components/matters-overview/data-source.jsx
@@ -0,0 +1,45 @@
+export const matter = {
+    id: 1816,
+    name: 'Zeng 5626',
+    matter_number: '{Z6-1816/210}',
+    client_id: 210,
+    client_name: 'Zeng Zac Homes',
+    substantially_responsible: {
+        id: 2,
+        name: 'Adrian Silva',
+        email: 'adrian.silva@lophils.com',
+        profile_picture: 'https://as1.ftcdn.net/v2/jpg/00/53/01/86/1000_F_53018616_nxpsx4iXIGTMMkfCuusbrd0jIqupmhcP.jpg?auto=compress&cs=tinysrgb&h=650&w=940'
+    }
+}
+
+  export const witness_affidavits = [
+    {
+      id:1,
+      name: 'Jack Pipper Stone',
+      comments: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      rfi: {
+          id: 15,
+          name: 'Lorem Ipsum',
+      }
+    },
+    {
+      id:2,
+      name: 'Thomas Bevere',
+      comments: 'Nulla maximus fringilla tellus eget dapibus. Maecenas id leo sit amet lacus eleifend eleifend et in diam.',
+      rfi: {
+          id: 25,
+          name: 'Dolor sit amet',
+      }
+    },
+    {
+      id:3,
+      name: 'Hanz Allen',
+      comments: 'Vivamus sed pellentesque nunc, sit amet ultricies arcu. Suspendisse a vestibulum libero. Curabitur sed convallis lorem, nec volutpat massa.',
+      rfi: {
+          id: 18,
+          name: 'Nulla maximus fringilla',
+      }
+      
+    }
+  ]
+  
