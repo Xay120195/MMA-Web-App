c6ec6d0235936ee207825c04ef3989de0b78866f
Adrian <adrian.silva@lophils.com>
2021-11-17 10:58:37 +0800

    FEATURE: Link to Chronology - Add Dummy Data Source

---
+++ src/components/link-to-chronology/data-source.jsx
@@ -0,0 +1,35 @@
+export const chronology = [
+    {
+    id:1,
+    item: 1,
+    date: '2021-10-01',
+    description_of_background: 'Phasellus purus massa, ullamcorper sed tempor quis, sollicitudin a mauris.',
+    filename: 'Matter 1_Payment Schedule for Claim No. 58711308181433_100521 03_58AM.pdf',
+    category: {
+        id: 15,
+        name: 'Lorem Ipsum Phasellus purus massa'
+    }
+    },
+    {
+    id:2,
+    item: 2,
+    date: '2021-09-12',
+    description_of_background: 'Morbi facilisis lectus sed venenatis tempus.',
+    filename: 'Matter 1_Achieve better email deliverability and higher security with DMARC Enforcement_110321 11_45AM.pdf',
+    category: {
+        id: 25,
+        name: 'Morbi facilisis lectus sed'
+    }
+    },
+    {
+    id:3,
+    item: 3,
+    date: '2021-09-24',
+    description_of_background: 'Cras ac varius ligula. Aenean nisi erat',
+    filename: 'Matter 1_Achieve better email deliverability and higher security with DMARC Enforcement_110321 11_45AM.pdf',
+    category: {
+        id: 15,
+        name: 'Lorem Ipsum Phasellus purus massa'
+    }
+    }
+]
