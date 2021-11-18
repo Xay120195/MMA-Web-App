f73d396d118cdf27969ec16128e43e65791d2b32
savestidolajr-lop <41558273+savestidolajr-lop@users.noreply.github.com>
2021-11-18 16:46:46 +0800

    FEATURE: Dashboard (delete function, toast and modal)

---
+++ src/components/dashboard/delete-matters-modal.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { GrClose } from "react-icons/gr";
+
+export default function DeleteMatterModal(props) {
+  
+
+  const handleModalClose = () => {
+    props.handleModalClose();
+  };
+
+  const handleSave = () => {
+      props.handleSave();
+  };
+
+
+  return (
+    <>
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="relative w-full my-6 mx-auto max-w-xl">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+              <h3 className="text-2xl font-semibold">Are you sure do you want delete this matter?</h3>
+              <button
+                className="p-1 ml-auto bg-transparent border-0 text-black opacity-4 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                onClick={handleModalClose}
+              >
+                <GrClose />
+              </button>
+            </div>
+            
+            <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+              <button
+                className="bg-gray-200 text-black active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={handleModalClose}
+              >
+                No
+              </button>
+              <button
+                className="bg-red-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={() => handleSave()}
+              >
+                Yes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+    </>
+  );
+}
