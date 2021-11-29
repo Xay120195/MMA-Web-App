f2888850eac4cb72ce60da8ad91eeb9408c8fa8b
JM Valino <jcvalino@apper.ph>
2021-11-29 16:52:30 +0800

    wip: lambda resolver example calling ddb

---
+++ packages/api/src/handlers/graphql/company/index.js
@@ -0,0 +1,41 @@
+const client = require('../../../lib/dynamodb-client')
+const {
+    PutItemCommand
+  } = require('@aws-sdk/client-dynamodb')
+const { marshall } = require('@aws-sdk/util-dynamodb');
+const { v4 } = require('uuid');
+
+ async function createCompany(data) {
+    const params = {
+      id: v4(),
+      name: data.name,
+      phone: data.phone,
+    };
+    const command = new PutItemCommand({
+      TableName: 'CompanyTable',
+      Item: marshall(params),
+    });
+    await client.send(command);
+    return params;
+  }
+
+
+  const resolvers = {
+    Mutation: {
+      companyCreate: async (ctx) => {
+        return createCompany(ctx.arguments)
+      },
+    }
+}
+  
+
+exports.handler = async (ctx) => {
+    const typeHandler = resolvers[ctx.info.parentTypeName];
+    if (typeHandler) {
+        const resolver = typeHandler[ctx.info.fieldName];
+        if (resolver) {
+        return await resolver(ctx);
+        }
+    }
+    throw new Error("Resolver not found.");
+}
