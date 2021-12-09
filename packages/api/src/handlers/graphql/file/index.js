925f7f268dbb27de66f2ac89190e77158d5710e5
JM Valino <jcvalino@apper.ph>
2021-12-09 19:05:30 +0800

    demo: pre-signed-urls

---
+++ packages/api/src/handlers/graphql/file/index.js
@@ -0,0 +1,21 @@
+const { generatePresignedUrl} = require('../../../services/MatterService')
+
+const resolvers = {
+  File: {
+    downloadURL: async (ctx) => {
+      return generatePresignedUrl(ctx.source.s3ObjectKey);
+    },
+  }
+  
+};
+
+exports.handler = async (ctx) => {
+  const typeHandler = resolvers[ctx.info.parentTypeName];
+  if (typeHandler) {
+    const resolver = typeHandler[ctx.info.fieldName];
+    if (resolver) {
+      return await resolver(ctx);
+    }
+  }
+  throw new Error("Resolver not found.");
+};
