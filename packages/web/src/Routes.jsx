6b537e909015684ef377510c0eedd695f2b15e77
Adrian <adrian.silva@lophils.com>
2021-12-13 17:31:07 +0800

    FEATURE: Run Navigation Component inside app only

--- packages/web/src/Routes.jsx
+++ packages/web/src/Routes.jsx
@@ -9,28 +9,26 @@ import Profile from "./components/user-profile";
 import ChangePassword from "./components/change-password";
 import PostRegistration from "./components/authentication/post-registration";
 import PostAuthentication from "./components/authentication/post-authentication";
-
 import { AppRoutes } from "./constants/AppRoutes";
-
-import Navbar from './components/navigation';
+import Navbar from "./components/navigation";
 
 const Routes = () => {
   return (
     <Router>
-      
-      <Navbar />
       <Switch>
-        <Route exact path='/' component={Authentication} />
+        <Route exact path="/" component={Authentication} />
         <Route exact path={AppRoutes.POSTREGISTRATION} component={PostRegistration} />
         <Route exact path={AppRoutes.POSTAUTHENTICATION} component={PostAuthentication} />
-        <Route exact path={AppRoutes.DASHBOARD} component={Dashboard} />
-        <Route exact path={`${AppRoutes.MATTERSOVERVIEW}/:id`} component={MattersOverview} />
-        <Route exact path={AppRoutes.USERTYPEACCESS} component={UserTypeAccess} />
-        <Route exact path={`${AppRoutes.WITNESSAFFIDAVIT}/:id`} component={WitnessAffidavit} />
-        <Route exact path={`${AppRoutes.MATTERSRFI}/:id`} component={MattersRFI} />
-        <Route exact path={AppRoutes.PROFILE} component={Profile} />
-        <Route exact path={AppRoutes.CHANGEPASSWORD} component={ChangePassword} />
-        
+        <>
+          <Navbar />
+          <Route exact path={AppRoutes.DASHBOARD} component={Dashboard} />
+          <Route exact path={`${AppRoutes.MATTERSOVERVIEW}/:id`} component={MattersOverview} />
+          <Route exact path={AppRoutes.USERTYPEACCESS} component={UserTypeAccess} />
+          <Route exact path={`${AppRoutes.WITNESSAFFIDAVIT}/:id`} component={WitnessAffidavit} />
+          <Route exact path={`${AppRoutes.MATTERSRFI}/:id`} component={MattersRFI} />
+          <Route exact path={AppRoutes.PROFILE} component={Profile} />
+          <Route exact path={AppRoutes.CHANGEPASSWORD} component={ChangePassword} />
+        </>
       </Switch>
     </Router>
   );
--- packages/web/src/components/dashboard/welcome.jsx
+++ packages/web/src/components/dashboard/welcome.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 export const Welcome = (props) => {
+    console.log(props);
     return(
         <>
         <h3 className="font-semi-bold text-3xl">
-            Welcome Back, <span className="font-bold text-3xl">{props.user.attributes.given_name }</span> 
+            Welcome Back, <span className="font-bold text-3xl">{props.user.firstName }</span> 
         </h3>
         <p className="text-gray-400 text-sm mt-2">
             You have <span className="font-bold">{props.matters.length}</span> Matters on your list.
