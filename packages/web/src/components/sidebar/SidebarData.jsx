0c9fc9a32af5daefbcb452be4cb5b540e94e71f1
Adrian <adrian.silva@lophils.com>
2022-01-12 17:24:09 +0800

    FEATURE: User Access - Implement ACL in Sidebar Navigation

--- packages/web/src/components/sidebar/SidebarData.jsx
+++ packages/web/src/components/sidebar/SidebarData.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faTachometer, faBooks, faUsers, faUsersCog } from '@fortawesome/pro-light-svg-icons';
 import { FaUsersCog, FaUserCog, FaTachometerAlt } from "react-icons/fa";
@@ -6,28 +6,72 @@ import { CgProfile } from "react-icons/cg";
 
 import { AppRoutes } from "../../constants/AppRoutes";
 
-export const SidebarData = [
+const access = JSON.parse(localStorage.getItem("access"));
+
+const staticPages = [
   {
-    title: 'Dashboard',
-    path: AppRoutes.DASHBOARD,
-    icon: <FaTachometerAlt style={{ color: 'var(--mysteryGrey)' }} />,
-    cName: 'nav-text'
-  },{
-    title: 'Users Access',
-    path: AppRoutes.USERTYPEACCESS,
-    icon: <FaUsersCog style={{ color: 'var(--mysteryGrey)' }} />,
-    cName: 'nav-text'
-  },
-  {
-    title: 'Account Settings',
+    order: 3,
+    title: "Account Settings",
     path: AppRoutes.ACCOUNTSETTINGS,
-    icon: <FaUserCog style={{ color: 'var(--mysteryGrey)' }} />,
-    cName: 'nav-text'
-  }
-  // {
-  //   title: 'Profile',
-  //   path: AppRoutes.PROFILE,
-  //   icon: <CgProfile style={{ color: 'var(--mysteryGrey)' }} />,
-  //   cName: 'nav-text'
-  // }
+    icon: <FaUserCog style={{ color: "var(--mysteryGrey)" }} />,
+    cName: "nav-text",
+  },
 ];
+
+const taggedPages = access.filter(
+  (page) => page.name === "DASHBOARD" || page.name === "USERTYPEACCESS"
+);
+
+// for (var i = 0; i < taggedPages.length; i++) {
+//   if (taggedPages[i].name === "DASHBOARD") {
+//     arrayAccess.push({
+//       order: 1,
+//       title: "Dashboard",
+//       path: AppRoutes.DASHBOARD,
+//       icon: <FaTachometerAlt style={{ color: "var(--mysteryGrey)" }} />,
+//       cName: "nav-text",
+//     });
+//   }
+
+//   if (taggedPages[i].name === "USERTYPEACCESS") {
+//     arrayAccess.push({
+//       order: 2,
+//       title: "Users Access",
+//       path: AppRoutes.USERTYPEACCESS,
+//       icon: <FaUsersCog style={{ color: "var(--mysteryGrey)" }} />,
+//       cName: "nav-text",
+//     });
+//   }
+// }
+
+let arrayAccess = taggedPages.map((pages) => {
+  let acs = {};
+
+  if (pages.name === "DASHBOARD") {
+    acs = {
+      order: 1,
+      title: "Dashboard",
+      path: AppRoutes.DASHBOARD,
+      icon: <FaTachometerAlt style={{ color: "var(--mysteryGrey)" }} />,
+      cName: "nav-text",
+    };
+  }
+
+  if (pages.name === "USERTYPEACCESS") {
+    acs = {
+      order: 2,
+      title: "Users Access",
+      path: AppRoutes.USERTYPEACCESS,
+      icon: <FaUsersCog style={{ color: "var(--mysteryGrey)" }} />,
+      cName: "nav-text",
+    };
+  }
+
+  return acs;
+});
+
+arrayAccess = arrayAccess
+  .concat(staticPages)
+  .sort((a, b) => (a.order > b.order ? 1 : -1));
+console.log(arrayAccess);
+export const SidebarData = arrayAccess;
