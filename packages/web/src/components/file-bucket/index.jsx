18e720803784248fe1ca4117962c43cb471dc47a
kjmfrias-lop <krizia.frias@lophils.com>
2022-02-03 16:42:46 +0800

    FEATURE: File Bucket - Display blank state when no records found and Display records in table

--- packages/web/src/components/file-bucket/index.jsx
+++ packages/web/src/components/file-bucket/index.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import ToastNotification from "../toast-notification";
 import { API } from "aws-amplify";
 import { useForm } from "react-hook-form";
+import BlankState from "../blank-state";
 
 import { Link } from "react-router-dom";
 import { MdArrowForwardIos } from "react-icons/md";
+import { AiOutlineDownload } from "react-icons/ai";
 import { FiUpload } from "react-icons/fi";
 
 import '../../assets/styles/BlankState.css';
@@ -92,17 +94,17 @@ export default function FileBucket() {
 
   const dummyData = [
     {
-      "fileName": "Test1",
+      "fileName": "Urgent Follow-up Email.pdf",
       "fileType": "pdf",
       "fileSize": "10kb"
     },
     {
-      "fileName": "Test2",
+      "fileName": "Urgent Follow-up Email.pdf",
       "fileType": "pdf",
       "fileSize": "10kb"
     },
     {
-      "fileName": "Test3",
+      "fileName": "Urgent Follow-up Email.pdf",
       "fileType": "pdf",
       "fileSize": "10kb"
     }
@@ -157,102 +159,49 @@ export default function FileBucket() {
   
     {dummyData != undefined ? ( 
       <>
-          <div className="p-5 px-5 py-1 left-0">
-              <p className={"text-lg mt-3 font-medium"}>FILES</p>
-          </div>
-          <div className="p-5 px-5 py-1 left-0">
+        <div className="p-5 px-5 py-1 left-0">
+            <p className={"text-lg mt-3 font-medium"}>FILES</p>
+        </div>
+        <div className="p-5 px-5 py-1 left-0">
             <div className="w-full h-42 bg-gray-100 rounded-lg border border-gray-200 mb-6 py-1 px-1">
-              <div className="BlankState">
-                  <div className="main-content">
-                    <div className="img-content">
-                      <img src={Illustration} alt="" />
-                    </div>
-                    <div className="details-txt">
-                      <h1>You have not uploaded any files yet.</h1>
-
-                    </div>
-                  </div>
-              </div>
+            <BlankState
+              title={"items"}
+              txtLink={"file upload button"}
+              handleClick={"insertfunctionhere"}/>
             </div>
-          </div>
+        </div>
       </>
     ) : (
       <>
       <div className="mt-7">
-      <div>
-                <input
-                  type="checkbox"
-                  name="check_all"
-                  id="check_all"
-                  className="cursor-pointer mr-2"
-                />
-                <button
-                    className="bg-green-400 hover:bg-green-500 text-white text-sm py-2 px-4 rounded inline-flex items-center border-0 shadow outline-none focus:outline-none focus:ring"
-                >
-                    Add Row &nbsp;
-                    <HiOutlinePlusCircle />
-                </button>
-                <button className="bg-gray-50 hover:bg-gray-100 text-black text-sm py-2 px-4 rounded inline-flex items-center border-0 shadow outline-none focus:outline-none focus:ring ml-2">
-                    Filter by Client &nbsp;
-                    <HiOutlineFilter />
-                </button>
-                <button
-                    className="bg-red-400 hover:bg-red-500 text-white text-sm py-2 px-4 rounded inline-flex items-center border-0 shadow outline-none focus:outline-none focus:ring ml-2"
-                  >
-                    Delete &nbsp;
-                    <HiTrash />
-                </button>
-
-                <input
-                  type="search"
-                  placeholder="Search ..."
-                  className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring pl-5 float-right w-3/12"
-                />
+      <div className="p-5 px-5 py-0 left-0">
+        <p className={"text-lg mt-3 font-medium"}>FILES</p>
       </div>
-
       <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg my-5">
             <table className="min-w-full divide-y divide-gray-200">
               <thead>
                 <tr>
-                {tableHeaders.map((header, index) => (
-                    <th
-                      key={index}
-                      scope="col"
-                      className="px-6 py-3 font-medium text-gray-500 tracking-wider"
-                    >
-                      {header}
-                    </th>
-                  ))}
+                  <th className="px-6 py-4 whitespace-nowrap w-4 text-left">Name </th>
                 </tr>
               </thead>
               <tbody className="bg-white divide-y divide-gray-200">
                {dummyData
-                  .map((wa, index) => (
+                  .map((ddata, index) => (
                     <tr key={index} index={index}>
-                      <td className="px-6 py-4 w-10 align-top place-items-center"> <input
-                          type="checkbox" className="cursor-pointer"/></td>
-                      <td className="px-6 py-4 whitespace-nowrap w-4 text-center"> {wa.fileName}</td>
-                      <td className="px-6 py-4 whitespace-nowrap w-4 text-center"> {wa.fileType}</td>
-                      <td className="px-6 py-4 whitespace-nowrap w-4 text-center"> {wa.fileSize}</td>
-                      <td className="px-6 py-4 whitespace-nowrap w-4 text-center"> </td>
-
+                      <td className="px-6 py-4 w-10 align-top place-items-center"> 
+                      <div>
+                        <span>{ddata.fileName} </span>
+                        <span className="absolute right-20"><AiOutlineDownload className="text-blue-400" /></span>
+                      </div>
+                      </td> 
                     </tr>
                   ))}
-
               </tbody>
             </table>
       </div>
-
-
-
       </div>
-      
-      
       </>
-
-
     )}
-   
   </div>
  
 
