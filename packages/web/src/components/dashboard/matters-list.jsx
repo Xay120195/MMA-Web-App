14758386acc6380b6b5495053a8e0c51835a78bf
savestidolajr-lop <41558273+savestidolajr-lop@users.noreply.github.com>
2022-02-09 09:59:20 +0800

    Quick Update: Adding Ellipsis and redirect filebucket/background

--- packages/web/src/components/dashboard/matters-list.jsx
+++ packages/web/src/components/dashboard/matters-list.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { AppRoutes } from "../../constants/AppRoutes";
 import dateFormat from "dateformat";
+import "../../assets/styles/Dashboard.css";
+import * as FaIcons from "react-icons/fa";
 
 export function MattersList({
   matter,
@@ -24,32 +26,22 @@ export function MattersList({
       {view === "grid" ? (
         <div className="w-full h-42 bg-gray-100 rounded-lg border border-gray-200 mb-6 py-5 px-4">
           <div>
-            {showDeleteMatter && (
-              <button
-                className="p-1 ml-auto bg-transparent border-0 text-black opacity-4 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                onClick={() => setshowDeleteModal(true)}
-              >
-                <svg
-                  stroke="currentColor"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  height="1em"
-                  width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill="none"
-                    stroke="#000"
-                    d="M3,3 L21,21 M3,21 L21,3"
-                  ></path>
-                </svg>
-              </button>
-            )}
-            <div>
-              {allowOpenFileBucket ? (
-                <Link to={`${AppRoutes.FILEBUCKET}/${matter.id}`}>File Bucket</Link>
-              ) : null}
+            {allowOpenFileBucket ? (
+            <div className="p-1 ml-auto bg-transparent border-0 text-black opacity-4 float-right text-3xl leading-none font-semibold outline-none focus:outline-none">
+              <div className="dropdown">
+                  <button className="bg-gray-100 text-gray-700 font-semibold rounded inline-flex">
+                  <FaIcons.FaEllipsisV />
+                  </button>
+                  <ul className="dropdown-menu absolute hidden text-gray-700 pt-1 bg-white p-2 font-semibold rounded">
+                    <li className="p-2"><Link to={`${AppRoutes.FILEBUCKET}/${matter.id}`}>File Bucket</Link></li>
+                    <li className="p-2"><Link to={`${AppRoutes.BACKGROUND}/${matter.id}`}>Background</Link></li>
+                    <li className="p-2" onClick={() => setshowDeleteModal(true)} ><a href="#">Archive</a></li>
+                  </ul>
+              </div>
+            </div>
+            ) : null}
 
+            <div>
               <h4
                 tabIndex="0"
                 className="focus:outline-none text-gray-800 dark:text-gray-100 font-bold mb-3"
