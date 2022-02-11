a1691bf25b7dd02aeef70007b62633eb13ece7f9
Adrian <adrian.silva@lophils.com>
2022-02-11 17:43:22 +0800

    FEATURE: Contacts - prepare backend query

--- packages/web/src/components/contacts/index.jsx
+++ packages/web/src/components/contacts/index.jsx
@@ -19,20 +19,29 @@ import {
 } from "react-icons/hi";
 import AddContactModal from "./add-contact-modal";
 
-
-
 export default function Contacts() {
   const [showAddContactModal, setshowAddContactModal] = useState(false);
   const handleModalClose = () => {
     setshowAddContactModal(false);
   };
 
+  const [contacts, setContacts] = useState(null);
   const [showToast, setShowToast] = useState(false);
   const [resultMessage, setResultMessage] = useState("");
   const hideToast = () => {
     setShowToast(false);
   };
 
+  const qGetContacts = `
+  query getMatterFile($matterId: ID) {
+    matterFile(matterId: $matterId) {
+      id
+      name
+      downloadURL
+      size
+      type
+    }
+  }`;
 
   const mInviteUser = `
       mutation inviteUser ($email: AWSEmail, $firstName: String, $lastName: String, $userType: UserType, $company: CompanyInput) {
@@ -51,6 +60,61 @@ export default function Contacts() {
         }
       }
   `;
+
+  useEffect(() => {
+    if (contacts === null) {
+      getContacts();
+    }
+  }, [contacts]);
+
+  let getContacts = async () => {
+    const params = {
+      query: qGetContacts,
+      variables: {
+        matterId: "6956",
+      },
+    };
+
+    await API.graphql(params).then((files) => {
+      console.log(files.data.matterFile);
+      const data = [
+        {
+          id: 0,
+          firstName: "Krizia",
+          lastName: "Frias",
+          userType: "Employee",
+          email: "km.frias@gmail.com",
+          company: { cid: "143", cname: "Lophils" },
+        },
+        {
+          id: 1,
+          firstName: "jane",
+          lastName: "Frias",
+          userType: "Employee",
+          email: "km.frias@gmail.com",
+          company: { cid: "143", cname: "Lophils" },
+        },
+        {
+          id: 2,
+          firstName: "kriz",
+          lastName: "Frias",
+          userType: "Employee",
+          email: "km.frias@gmail.com",
+          company: { cid: "143", cname: "Lophils" },
+        },
+        {
+          id: 3,
+          firstName: "krizi",
+          lastName: "Frias",
+          userType: "Employee",
+          email: "km.frias@gmail.com",
+          company: { cid: "143", cname: "Lophils" },
+        },
+      ]; //dummy data
+      //setContacts(files.data.matterFile);
+      setContacts(data);
+    });
+  };
 
   const {
     register,
@@ -118,48 +182,14 @@ export default function Contacts() {
     gridtemplatecolumn: "1fr auto",
   };
 
-  const data = [
-    {
-      "id": 0,
-      "firstName": "Krizia",
-      "lastName": "Frias",
-      "userType": "Employee",
-      "email": "km.frias@gmail.com",
-      "company": {"cid": "143", "cname": "Lophils"}
-    },
-    {
-      "id": 1,
-      "firstName": "jane",
-      "lastName": "Frias",
-      "userType": "Employee",
-      "email": "km.frias@gmail.com",
-      "company": {"cid": "143", "cname": "Lophils"}
-    },
-    {
-      "id": 2,
-      "firstName": "kriz",
-      "lastName": "Frias",
-      "userType": "Employee",
-      "email": "km.frias@gmail.com",
-      "company": {"cid": "143", "cname": "Lophils"}
-    },
-    {
-      "id": 3,
-      "firstName": "krizi",
-      "lastName": "Frias",
-      "userType": "Employee",
-      "email": "km.frias@gmail.com",
-      "company": {"cid": "143", "cname": "Lophils"}
-    }
-  ]; //dummy data
+  
 
-  const [contacts, setContacts] = useState(data);
   const [addFormData, setAddFormData] = useState({
     firstName: "",
     lastName: "",
     userType: "",
     email: "",
-    company: {cid: "", cname: ""},
+    company: { cid: "", cname: "" },
   });
 
   const handleAddFormChange = (event) => {
@@ -187,14 +217,12 @@ export default function Contacts() {
       lastName: addFormData.lastName,
       userType: addFormData.userType,
       email: addFormData.email,
-      company: {cid: addFormData.cid, cname: addFormData.cname},
+      company: { cid: addFormData.cid, cname: addFormData.cname },
     };
 
     const newContacts = [...contacts, newContact];
-    setContacts(newContacts);
+    //setContacts(newContacts);
   };
-
-
 
   const handleAddContact = () => {
     handleModalClose();
@@ -202,10 +230,12 @@ export default function Contacts() {
 
   return (
     <>
-    <div className={
+      <div
+        className={
           "p-5 relative flex flex-col min-w-0 break-words mb-6 shadow-lg rounded bg-white"
-        } style={contentDiv}>
-
+        }
+        style={contentDiv}
+      >
         <div className="relative flex-grow flex-1">
           <div style={mainGrid}>
             <div>
@@ -229,108 +259,113 @@ export default function Contacts() {
         </div>
 
         <div className="p-5 left-0">
-              <div>
-                <input
-                  type="checkbox"
-                  name="check_all"
-                  id="check_all"
-                  className="cursor-pointer mr-2"
-                  // checked={checkAllState}
-                  // onChange={(e) => handleCheckAllChange(e.target.checked)}
-                />
-                {/* {showAddRow && ( */}
-                  <button
-                    className="bg-green-400 hover:bg-green-500 text-white text-sm py-1 px-4 rounded inline-flex items-center border-0 shadow outline-none focus:outline-none focus:ring"
-                    onClick={() => setshowAddContactModal(true)}
-                  >
-                    Add Contact &nbsp;
-                    <HiOutlinePlusCircle />
-                  </button>
-                {/* )} */}
-                {/* {showExport && ( */}
-                  <button className="bg-gray-50 hover:bg-gray-100 text-black text-sm py-1 px-4 rounded inline-flex items-center border-0 shadow outline-none focus:outline-none focus:ring ml-2">
-                    Manage Filters &nbsp;
-                    <FiFilter/>
-                  </button>
-                {/* )} */}
-                {/* {showDeleteRow && ( */}
-                  <button
-                    className="bg-red-400 hover:bg-red-500 text-white text-sm py-1 px-2 rounded inline-flex items-center border-0 shadow outline-none focus:outline-none focus:ring ml-2"
-                    // onClick={() => handleDeleteRow(this)}
-                  >
-                    Delete &nbsp;
-                    <HiTrash />
-                  </button>
-                {/* )} */}
-              </div>
+          <div>
+            <input
+              type="checkbox"
+              name="check_all"
+              id="check_all"
+              className="cursor-pointer mr-2"
+              // checked={checkAllState}
+              // onChange={(e) => handleCheckAllChange(e.target.checked)}
+            />
+            {/* {showAddRow && ( */}
+            <button
+              className="bg-green-400 hover:bg-green-500 text-white text-sm py-1 px-4 rounded inline-flex items-center border-0 shadow outline-none focus:outline-none focus:ring"
+              onClick={() => setshowAddContactModal(true)}
+            >
+              Add Contact &nbsp;
+              <HiOutlinePlusCircle />
+            </button>
+            {/* )} */}
+            {/* {showExport && ( */}
+            <button className="bg-gray-50 hover:bg-gray-100 text-black text-sm py-1 px-4 rounded inline-flex items-center border-0 shadow outline-none focus:outline-none focus:ring ml-2">
+              Manage Filters &nbsp;
+              <FiFilter />
+            </button>
+            {/* )} */}
+            {/* {showDeleteRow && ( */}
+            <button
+              className="bg-red-400 hover:bg-red-500 text-white text-sm py-1 px-2 rounded inline-flex items-center border-0 shadow outline-none focus:outline-none focus:ring ml-2"
+              // onClick={() => handleDeleteRow(this)}
+            >
+              Delete &nbsp;
+              <HiTrash />
+            </button>
+            {/* )} */}
           </div>
+        </div>
 
         <div className="p-5 left-0">
-          <div className= "grid grid-cols-4 gap-4" >
-            {contacts.map((contact) => (
-              <div className="w-full h-42 bg-gray-100 rounded-lg border border-gray-200 mb-6 py-5 px-4">
-                <div className=" py-1 text-right">
+          <div className="grid grid-cols-4 gap-4">
+            {(contacts !== null &&
+              contacts.length !== 0) &&
+              contacts.map((contact) => (
+                <div className="w-full h-42 bg-gray-100 rounded-lg border border-gray-200 mb-6 py-5 px-4">
+                  <div className=" py-1 text-right">
                     <div
-                    className="bg-white hover:bg-gray-100 text-black font-semibold py-2 px-4 rounded inline-flex items-center border-0 shadow outline-none focus:outline-none focus:ring"
-                    // onClick={() => handleAddRow()}
+                      className="bg-white hover:bg-gray-100 text-black font-semibold py-2 px-4 rounded inline-flex items-center border-0 shadow outline-none focus:outline-none focus:ring"
+                      // onClick={() => handleAddRow()}
                     >
-                      {contact.userType} &nbsp; <AiOutlineDown/>
-                    </div> &nbsp;
-
+                      {contact.userType} &nbsp; <AiOutlineDown />
+                    </div>{" "}
+                    &nbsp;
                     <div
-                    className="bg-blue-400 hover:bg-blue-500 text-white text-sm py-2 px-4 rounded inline-flex items-center border-0 shadow outline-none focus:outline-none focus:ring"
-                    // onClick={() => handleAddRow()}
+                      className="bg-blue-400 hover:bg-blue-500 text-white text-sm py-2 px-4 rounded inline-flex items-center border-0 shadow outline-none focus:outline-none focus:ring"
+                      // onClick={() => handleAddRow()}
                     >
-                      Send &nbsp; <FiSend/>
+                      Send &nbsp; <FiSend />
                     </div>
-                </div>
-                <div className="flex items-start py-3">
-                  <div className="py-4 px-1"><input
-                    type="checkbox"
-                    name="check_all"
-                    id="check_all"
-                    className="cursor-pointer mr-2"
-                  /></div>
-                  <div className="py-3">
-                    <FaUserCircle className="text-2xl"/>
                   </div>
-                  <div className="details-txt px-3">
-                    <div className="flex items-start"><p className="font-semibold text-black">{contact.firstName} {contact.lastName}</p> &nbsp; <p className="font-semibold"> {contact.company.cname}</p></div>
-                    <p>{contact.email}</p>
+                  <div className="flex items-start py-3">
+                    <div className="py-4 px-1">
+                      <input
+                        type="checkbox"
+                        name="check_all"
+                        id="check_all"
+                        className="cursor-pointer mr-2"
+                      />
+                    </div>
+                    <div className="py-3">
+                      <FaUserCircle className="text-2xl" />
+                    </div>
+                    <div className="details-txt px-3">
+                      <div className="flex items-start">
+                        <p className="font-semibold text-black">
+                          {contact.firstName} {contact.lastName}
+                        </p>{" "}
+                        &nbsp;{" "}
+                        <p className="font-semibold">
+                          {" "}
+                          {contact.company.cname}
+                        </p>
+                      </div>
+                      <p>{contact.email}</p>
+                    </div>
+
+                    <div className="right-0 py-3">
+                      <BiDotsVerticalRounded />
+                    </div>
                   </div>
 
-                  <div className="right-0 py-3">
-                     <BiDotsVerticalRounded/>
+                  <div className="py-5 text-center">
+                    <div>
+                      <p className="font-semibold text-slate-700  ">
+                        TAGGED MATTERS
+                      </p>
+                    </div>
                   </div>
                 </div>
-
-                <div className="py-5 text-center"> 
-                  
-                  <div><p className="font-semibold text-slate-700  ">TAGGED MATTERS</p></div>
-                  
-                </div>
-
-
-              </div>
-          
-            ))}
-
-      
-      
-      
+              ))}
+          </div>
         </div>
       </div>
-    </div>
 
-    {showAddContactModal && (
+      {showAddContactModal && (
         <AddContactModal
           handleSave={handleAddContact}
           handleModalClose={handleModalClose}
         />
-    )}
-    
-    
-
+      )}
     </>
   );
 }
