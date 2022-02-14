847e39821f1b7808edf267df4b86904b19a23484
isf1-018 <99152878+isf1-018@users.noreply.github.com>
2022-02-14 16:26:58 +0800

    FEAT: Background Page Additional Feature Search filter and  Upload modal
    Additional feature search filter and click upload button will open the drag and drop upload modal

--- packages/web/src/components/background/action-buttons.jsx
+++ packages/web/src/components/background/action-buttons.jsx
@@ -109,7 +109,7 @@ const ActionButtons = ({
             Add row
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 mx-1"
+              className="h-4 w-4 mx-1"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -118,6 +118,7 @@ const ActionButtons = ({
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth={2}
+                d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
           </button>
