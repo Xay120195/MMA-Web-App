962dacdb28dd20ac8df4e9d5efcf4214f601a969
isf1-018 <99152878+isf1-018@users.noreply.github.com>
2022-02-22 08:40:27 +0800

    Feat: Revision Background Page
    this is for the revision of background page

--- packages/web/src/components/background/table-info.jsx
+++ packages/web/src/components/background/table-info.jsx
@@ -116,25 +116,25 @@ const TableInfo = ({
                         <tr>
                           <th
                             scope="col"
-                            className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                           >
                             No
                           </th>
                           <th
                             scope="col"
-                            className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                           >
                             Date
                           </th>
                           <th
                             scope="col"
-                            className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                           >
                             Description of Background
                           </th>
                           <th
                             scope="col"
-                            className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                           >
                             Document
                           </th>
@@ -143,14 +143,14 @@ const TableInfo = ({
                       <tbody className="bg-white divide-y divide-gray-200">
                         {witness.map((item, index) => (
                           <tr key={index}>
-                            <td className="px-3">
+                            <td className="px-3 py-3 w-10">
                               <div className="flex items-center ">
                                 <input
                                   type="checkbox"
                                   value={`${item.id}`}
                                   name={`${item.id}`}
                                   id={`${item.id}`}
-                                  className="cursor-pointer"
+                                  className="cursor-pointer w-10"
                                   checked={checkedState[index]}
                                   onChange={(event) =>
                                     handleCheckboxChange(index, event)
@@ -158,16 +158,16 @@ const TableInfo = ({
                                 />
                                 <label
                                   htmlFor="checkbox-1"
-                                  className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                  className="text-sm font-medium text-gray-900 dark:text-gray-300"
                                 >
                                   {item.id}
                                 </label>
                               </div>
                             </td>
-                            <td className="px-1">
+                            <td className="px-3 py-3">
                               <div>
                                 <DatePicker
-                                  className="border rounded border-gray-300"
+                                  className="border w-28 rounded border-gray-300"
                                   selected={new Date(item.date)}
                                   onChange={(date) =>
                                     handleChangeDate(date, item.id)
@@ -175,7 +175,7 @@ const TableInfo = ({
                                 />
                               </div>
                             </td>
-                            <td className="py-2 px-3 ">
+                            <td className="py-2 px-3 w-full">
                               <ContentEditable
                                 html={item.comments}
                                 className="w-full mx-4 px-3"
@@ -183,11 +183,11 @@ const TableInfo = ({
                                 onBlur={HandleChangeToTD}
                               />
                             </td>
-                            <td className="py-2 px-3 w-60 text-sm text-gray-500">
+                            <td className="py-2 px-3 w-80 text-sm text-gray-500">
                               <button
                                 type="submit"
                                 onClick={() => setShowUpload(true)}
-                                className=" w-full bg-green-400 border border-transparent rounded-md py-2 px-4 flex items-center justify-center text-base font-medium text-white hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                className=" w-60 bg-green-400 border border-transparent rounded-md py-2 px-4 flex items-center justify-center text-base font-medium text-white hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                               >
                                 Upload
                                 <svg
