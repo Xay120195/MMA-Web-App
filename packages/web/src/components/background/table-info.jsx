85373197c0e74d26ccc596f9e9d98b97df12ecbc
savestidolajr-lop <41558273+savestidolajr-lop@users.noreply.github.com>
2022-04-01 11:18:53 +0800

    UPDATE: Map Items for background

--- packages/web/src/components/background/table-info.jsx
+++ packages/web/src/components/background/table-info.jsx
@@ -54,6 +54,10 @@ const TableInfo = ({
   checkDate,
   checkDesc,
   checkDocu,
+  pageTotal,
+  pageIndex,
+  pageSize,
+  pageSizeConst,
 }) => {
   let temp = selectedRowsBG;
   let tempFiles = selectedRowsBGFiles;
@@ -564,7 +568,8 @@ const TableInfo = ({
                             {...provider.droppableProps}
                             className="bg-white divide-y divide-gray-200"
                           >
-                            {witness.map((item, index) => (
+                            {/* {witness.map((item, index) => ( */}
+                            {witness.slice(pageIndex-1, pageSizeConst).map((item, index) => (
                               <Draggable
                                 key={item.id}
                                 draggableId={item.id}
