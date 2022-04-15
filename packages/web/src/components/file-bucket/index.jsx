511bd311dcccdc6191fe76cb28e40902e40baa9a
Adrian <adrian.silva@lophils.com>
2022-04-15 15:15:45 +0800

    TEST: Remove Lazy Load

--- packages/web/src/components/file-bucket/index.jsx
+++ packages/web/src/components/file-bucket/index.jsx
@@ -261,8 +261,8 @@ mutation tagFileLabel($fileId: ID, $labels: [LabelInput]) {
   `;
 
   const mPaginationbyItems = `
-query getFilesByMatter($isDeleted: Boolean, $limit: Int, $matterId: ID, $nextToken: String) {
-  matterFiles(isDeleted: $isDeleted, matterId: $matterId, nextToken: $nextToken, limit: $limit, sortOrder:CREATED_DESC) {
+query getFilesByMatter($isDeleted: Boolean, $matterId: ID) {
+  matterFiles(isDeleted: $isDeleted, matterId: $matterId, sortOrder:CREATED_DESC) {
     items {
       id
       name
@@ -1706,7 +1706,7 @@ query getFilesByMatter($isDeleted: Boolean, $limit: Int, $matterId: ID, $nextTok
                       </DragDropContext>
                     </div>
                     <div>
-                      {maxLoading ? (
+                      {/* {maxLoading ? (
                         <div className="flex justify-center items-center mt-5">
                           <p>All data has been loaded.</p>
                         </div>
@@ -1716,7 +1716,7 @@ query getFilesByMatter($isDeleted: Boolean, $limit: Int, $matterId: ID, $nextTok
                         </div>
                       ) : (
                         <span></span>
-                      )}
+                      )} */}
 
                       {!maxLoading && loading ? (
                         <span className="grid"></span>
