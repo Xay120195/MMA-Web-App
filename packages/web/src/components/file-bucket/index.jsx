cf4d79c33affc15d4eeb104f1c8d2c3c755420dd
kjmfrias-lop <krizia.frias@lophils.com>
2022-05-05 14:21:07 +0800

    UPDATE: File bucket - filter labels from database

--- packages/web/src/components/file-bucket/index.jsx
+++ packages/web/src/components/file-bucket/index.jsx
@@ -1179,8 +1179,42 @@ query getFilesByMatter($isDeleted: Boolean, $matterId: ID) {
     }
   };
 
+  const mGetFilesByLabel = `
+    query getFilesByLabel($id: [ID]) {
+      multipleLabels(id: $id) {
+        files {
+        items {
+          id
+          name
+          details
+          date
+          s3ObjectKey
+          labels {
+            items {
+              id
+              name
+            }
+          }
+          backgrounds {
+            items {
+              id
+              order
+              description
+            }
+          }
+          createdAt
+          order
+          type
+          size
+        }
+        nextToken
+        }
+      }
+    }
+    `;
+
   //filter function
-  const handleFilter = (fileFilter) => {
+  const handleFilter = async (fileFilter) => {
     console.log("ff", fileFilter);
     console.log("filesToFilter", matterFiles);
     setFilterLabels(false);
@@ -1197,25 +1231,63 @@ query getFilesByMatter($isDeleted: Boolean, $matterId: ID) {
       // setFiles(sortByOrder(matterFiles));
     } else {
       // getMatterFiles(next);
-      console.log("files", files);
+      // console.log("files", files);
+      // for (var i = 0; i < fileFilter.length; i++) {
+      //   files.map((x) =>
+      //     x.labels.items !== null
+      //       ? x.labels.items.map((y) =>
+      //           y.name === fileFilter[i]
+      //             ? (filterRecord = [...filterRecord, x])
+      //             : (filterRecord = filterRecord)
+      //         )
+      //       : x.labels.items
+      //   );
+      // }
+
+      // var listFilter = [
+      //   ...new Map(filterRecord.map((x) => [JSON.stringify(x), x])).values(),
+      // ];
+      // console.log(listFilter);
+      // setMatterFiles(sortByOrder(listFilter));
+      // setFiles(sortByOrder(listFilter));
+
+      console.log("labels", labels);
+      var labelsList = labels;
+      var labelsIdList = [];
+
       for (var i = 0; i < fileFilter.length; i++) {
-        files.map((x) =>
-          x.labels.items !== null
-            ? x.labels.items.map((y) =>
-                y.name === fileFilter[i]
-                  ? (filterRecord = [...filterRecord, x])
-                  : (filterRecord = filterRecord)
-              )
-            : x.labels.items
+        labelsList.map((x) => 
+          x.label === fileFilter[i] ? 
+          labelsIdList = [...labelsIdList, x.value] :
+          labelsIdList = labelsIdList 
         );
       }
 
-      var listFilter = [
-        ...new Map(filterRecord.map((x) => [JSON.stringify(x), x])).values(),
+      var uniqueIds = [
+        ...new Map(labelsIdList.map((x) => [JSON.stringify(x), x])).values(),
       ];
-      console.log(listFilter);
-      setMatterFiles(sortByOrder(listFilter));
-      // setFiles(sortByOrder(listFilter));
+
+      console.log("labelIds", uniqueIds);
+
+      const result = await API.graphql({
+        query: mGetFilesByLabel,
+        variables: {
+          id: uniqueIds,
+        },
+      });
+
+      var newFiles = result.data.multipleLabels;
+
+      var newFiles1 = [];
+      var newFiles2 = [];
+      result.data.multipleLabels.map((x) => newFiles1 = [...newFiles1, x.files.items]);
+      newFiles1.map((x) => x.map((y)=> newFiles2 = [...newFiles2, y]));
+
+      console.log("putinmatterfiles",newFiles2);
+      setMatterFiles(sortByOrder(newFiles2));
+      setFiles(sortByOrder(newFiles2));
+
+      console.log("res", result);
     }
   };
 
