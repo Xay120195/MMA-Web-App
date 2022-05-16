fb7c3165a718f5e26aa23d9da01928d8f3a80817
Adrian <adrian.silva@lophils.com>
2022-05-16 16:12:40 +0800

    FEATURE: Update Requests

--- packages/api/src/handlers/graphql/mutation/index.js
+++ packages/api/src/handlers/graphql/mutation/index.js
@@ -1174,6 +1174,42 @@ async function updateBrief(id, data) {
   return resp;
 }
 
+async function updateRequest(id, data) {
+  let resp = {};
+  try {
+    const {
+      ExpressionAttributeNames,
+      ExpressionAttributeValues,
+      UpdateExpression,
+    } = getUpdateExpressions(data);
+
+    const param = {
+      id,
+      ...data,
+    };
+
+    const cmd = new UpdateItemCommand({
+      TableName: "RequestTable",
+      Key: marshall({ id }),
+      UpdateExpression,
+      ExpressionAttributeNames,
+      ExpressionAttributeValues,
+    });
+
+    const request = await ddbClient.send(cmd);
+
+    resp = request ? param : {};
+  } catch (e) {
+    resp = {
+      error: e.message,
+      errorStack: e.stack,
+    };
+    console.log(resp);
+  }
+
+  return resp;
+}
+
 async function bulkUpdateBackgroundOrders(data) {
   let resp = [];
   try {
@@ -1726,6 +1762,22 @@ const resolvers = {
     },
     requestCreate: async (ctx) => {
       return await createRequest(ctx.arguments);
+    },
+    requestUpdate: async (ctx) => {
+      const { id, question, answer, itemNo, order } = ctx.arguments;
+      const data = {
+        updatedAt: new Date().toISOString(),
+      };
+
+      if (question !== undefined) data.question = question;
+
+      if (answer !== undefined) data.answer = answer;
+
+      if (itemNo !== undefined) data.itemNo = itemNo;
+
+      if (order !== undefined) data.order = order;
+
+      return await updateRequest(id, data);
     },
     briefCreate: async (ctx) => {
       return await createBrief(ctx.arguments);
