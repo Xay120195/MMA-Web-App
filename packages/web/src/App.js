2b545d628d95e4c1b8a6bf5a32a4a1b0d830c575
Adrian <adrian.silva@lophils.com>
2022-05-26 13:18:58 +0800

    Update App.js

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Routes from "./Routes";
 
-import {Amplify} from "aws-amplify";
+import Amplify, { Auth } from "aws-amplify";
 import {PubSub} from "aws-amplify";
 import awsmobile from "./aws-exports";
 import "./assets/styles/styles.css";
 Amplify.configure(awsmobile);
 PubSub.configure(awsmobile);
-
+Auth.configure(awsmobile);
 function App() {
   return <Routes />;
 }
