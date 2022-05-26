7f3e8c2269a6070ea79860fa0d6d0346f9e97708
jmvalino <jmvalinowapx@gmail.com>
2022-05-26 15:29:44 +0800

    removed @aws-amplify modules

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Routes from "./Routes";
 
-import Amplify from "@aws-amplify/core";
-import PubSub from "@aws-amplify/pubsub";
+import { Amplify } from "aws-amplify";
 import awsmobile from "./aws-exports";
+
 import "./assets/styles/styles.css";
 Amplify.configure(awsmobile);
-PubSub.configure(awsmobile);
-
 function App() {
   return <Routes />;
 }
