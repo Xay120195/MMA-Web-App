c56a7ad3aa4316985f6e063d2f9162bdcd36991b
Adrian <adrian.silva@lophils.com>
2022-05-26 15:14:56 +0800

    Install amplify auth

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Routes from "./Routes";
 
-import Amplify from "@aws-amplify/core";
-import PubSub from "@aws-amplify/pubsub";
+import {Amplify} from "@aws-amplify/core";
+import {PubSub} from "@aws-amplify/pubsub";
+import {Auth} from '@aws-amplify/auth'
 import awsmobile from "./aws-exports";
 import "./assets/styles/styles.css";
 Amplify.configure(awsmobile);
 PubSub.configure(awsmobile);
+Auth.configure(awsmobile);
 
 function App() {
   return <Routes />;
