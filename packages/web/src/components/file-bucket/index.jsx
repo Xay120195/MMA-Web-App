ec4923419d261da8c52f0392f4ef577d1887efef
kjmfrias-lop <krizia.frias@lophils.com>
2022-06-17 10:10:02 +0800

    UPDATE: File bucket - rename page ref column

--- packages/web/src/components/file-bucket/index.jsx
+++ packages/web/src/components/file-bucket/index.jsx
@@ -376,6 +376,12 @@ query getFilesByMatter($isDeleted: Boolean, $limit: Int, $matterId: ID, $nextTok
           order
           description
           date
+          briefs {
+            items {
+              id
+              name
+            }
+          }
         }
       }
       createdAt
@@ -1297,11 +1303,14 @@ query getFilesByMatter($isDeleted: Boolean, $limit: Int, $matterId: ID, $nextTok
       setResultMessage(`Successfully Deleted!`);
       setShowToast(true);
       setTimeout(() => {
-        getMatterFiles(1);
+        //getMatterFiles(1);
         setShowToast(false);
         setDeletingState(false);
       }, 3000);
     }, 1000);
+
+
+
   };
 
   const handleChageBackground = (id) => {
@@ -2137,9 +2146,6 @@ query getFilesByMatter($isDeleted: Boolean, $limit: Int, $matterId: ID, $nextTok
           id: id,
         })
       );
-
-      console.log("Brief ID:", backgroundRedirect.data.background.briefs.items);
-      console.log("Background ID:", backgroundId);
       setTimeout(() => {
         setShowToast(false);
         window.location.href = `${AppRoutes.BACKGROUND}/${matter_id}/${
@@ -2928,12 +2934,9 @@ query getFilesByMatter($isDeleted: Boolean, $limit: Int, $matterId: ID, $nextTok
                                                     <b>
                                                       {background.order +
                                                         1 +
-                                                        ". "}
+                                                        ". " + ellipsis(background.briefs.items[0].name, 40) }
                                                     </b>
-                                                    {ellipsis(
-                                                      getBriefName(background.id),
-                                                      40
-                                                    )}
+                                                   
                                                   </div>
                                                 ))}
                                             </div>
