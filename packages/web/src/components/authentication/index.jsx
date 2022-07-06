fd49197552e26a71b421c513ff6d8e8257e38dec
kjmfrias-lop <krizia.frias@lophils.com>
2022-07-06 10:00:08 +0800

    UPDATE: Login- add subheader

--- packages/web/src/components/authentication/index.jsx
+++ packages/web/src/components/authentication/index.jsx
@@ -247,7 +247,7 @@ const Authentication = () => {
           </>
         )}
       </Disclosure>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 bg-gray-100 sm:bg-white">
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-2  bg-gray-100 sm:bg-white">
         <div className="welcome-message">
           <h1>
             A Software Built for Managing Affidavits and Exchanging RFIs with
@@ -255,7 +255,7 @@ const Authentication = () => {
           </h1>
         </div>
 
-        <div className="authcontainer m-4">
+        <div className="authcontainer">
           <center>
             <h1 className="sm:hidden font-bold my-4">AFFIDAVITS &amp; RFI </h1>
           </center>
@@ -265,19 +265,25 @@ const Authentication = () => {
               slot="sign-in"
               headerText="Welcome Back!"
               formFields={AuthFields.login}
-            />
+              className="sm:w-full"
+            >
+            <div slot="header-subtitle">A Software Built for Managing Affidavits and Exchanging RFIs with
+            your Clients</div>
+            </AmplifySignIn>
             <AmplifySignUp
               id="amplify-sign-up"
               usernameAlias="email"
               slot="sign-up"
               formFields={signUpFields}
               headerText="Start Your Free Trial Now"
+              className="sm:w-full"
             />
             <AmplifyForgotPassword
               usernameAlias="email"
               slot="forgot-password"
               formFields={AuthFields.forgotpassword}
               headerText="Forgot Password"
+              className="sm:w-full"
             />
             {/* <AmplifyRequireNewPassword
               id="amplify-require-new-password"
