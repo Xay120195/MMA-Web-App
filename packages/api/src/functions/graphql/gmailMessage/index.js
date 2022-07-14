619be5480adc941f51f04b8fbf3fe59ba06fe9d8
Adrian <adrian.silva@lophils.com>
2022-07-14 23:30:31 +0800

    Add labels in Gmail Schema

--- packages/api/src/functions/graphql/gmailMessage/index.js
+++ packages/api/src/functions/graphql/gmailMessage/index.js
@@ -96,6 +96,81 @@ async function listGmailMessageClientMatters(ctx) {
   return response;
 }
 
+async function listGmailMessageLabels(ctx) {
+  const { id } = ctx.source;
+  const { limit, nextToken } = ctx.arguments;
+  try {
+    // const gmailClientMattersParam = {
+    //   TableName: "GmailMessageClientMatterTable",
+    //   IndexName: "byGmailMessage",
+    //   KeyConditionExpression: "gmailMessageId = :gmailMessageId",
+    //   ExpressionAttributeValues: marshall({
+    //     ":gmailMessageId": id,
+    //   }),
+    //   ScanIndexForward: false,
+    //   ExclusiveStartKey: nextToken
+    //     ? JSON.parse(Buffer.from(nextToken, "base64").toString("utf8"))
+    //     : undefined,
+    // };
+    // if (limit !== undefined) {
+    //   gmailClientMattersParam.Limit = limit;
+    // }
+    // const gmailClientMattersCmd = new QueryCommand(gmailClientMattersParam);
+    // const gmailClientMattersResult = await client.send(gmailClientMattersCmd);
+    // const clientMatterIds = gmailClientMattersResult.Items.map((i) =>
+    //   unmarshall(i)
+    // ).map((f) => marshall({ id: f.clientMatterId }));
+    // if (clientMatterIds.length !== 0) {
+    //   const clientMattersParam = {
+    //     RequestItems: {
+    //       ClientMatterTable: {
+    //         Keys: clientMatterIds,
+    //       },
+    //     },
+    //   };
+    //   const clientMattersCmd = new BatchGetItemCommand(clientMattersParam);
+    //   const clientMattersResult = await client.send(clientMattersCmd);
+    //   const objClientMatters =
+    //     clientMattersResult.Responses.ClientMatterTable.map((i) =>
+    //       unmarshall(i)
+    //     );
+    //   const objGmailClientMatters = gmailClientMattersResult.Items.map((i) =>
+    //     unmarshall(i)
+    //   );
+    //   const response = objGmailClientMatters
+    //     .map((item) => {
+    //       const filterMatter = objClientMatters.find(
+    //         (u) => u.id === item.clientMatterId
+    //       );
+    //       if (filterMatter !== undefined) {
+    //         return { ...item, ...filterMatter };
+    //       }
+    //     })
+    //     .filter((a) => a !== undefined);
+    //   return {
+    //     items: response,
+    //     nextToken: gmailClientMattersResult.LastEvaluatedKey
+    //       ? Buffer.from(
+    //           JSON.stringify(gmailClientMattersResult.LastEvaluatedKey)
+    //         ).toString("base64")
+    //       : null,
+    //   };
+    // } else {
+    //   return {
+    //     items: [],
+    //     nextToken: null,
+    //   };
+    // }
+  } catch (e) {
+    // response = {
+    //   error: e.message,
+    //   errorStack: e.stack,
+    // };
+    // console.log(response);
+  }
+  // return response;
+}
+
 async function listGmailMessageAttachments(ctx) {
   const { id } = ctx.source;
 
@@ -164,6 +239,9 @@ const resolvers = {
   GmailMessage: {
     clientMatters: async (ctx) => {
       return listGmailMessageClientMatters(ctx);
+    },
+    labels: async (ctx) => {
+      return listGmailMessageLabels(ctx);
     },
     attachments: async (ctx) => {
       return listGmailMessageAttachments(ctx);
