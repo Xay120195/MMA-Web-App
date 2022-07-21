012444f9d8b6e506607494ccce9c70a09cd2fc74
Adrian <adrian.silva@lophils.com>
2022-07-21 15:24:13 +0800

    Feature: Add Filter by Start Date End Date (Backend)

--- packages/api/src/functions/graphql/label/index.js
+++ packages/api/src/functions/graphql/label/index.js
@@ -103,8 +103,7 @@ const resolvers = {
 };
 
 exports.handler = async (ctx) => {
-  console.log("~aqs.watch:: run label >>", ctx.info.fieldName);
-  console.log("~aqs.watch:: arguments >>", ctx.arguments);
+  console.log("~aqs.watch:: run label >>", ctx.info.fieldName, ctx.arguments);
   const typeHandler = resolvers[ctx.info.parentTypeName];
   if (typeHandler) {
     const resolver = typeHandler[ctx.info.fieldName];
